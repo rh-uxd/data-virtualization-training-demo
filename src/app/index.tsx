1b5adae470845cd9bc685f77ccefa8f6b36c4a3b
Matt Nolting <matthew.nolting@gmail.com>
2020-05-31 22:57:45 -0400

    Add scss (#5)
    * feat(add-scss): added scss support

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import '@patternfly/react-core/dist/styles/base.css';
+import '@app/app.scss';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { AppLayout } from '@app/AppLayout/AppLayout';
 import { AppRoutes } from '@app/routes';
-import '@app/app.css';
 
 const App: React.FunctionComponent = () => (
   <Router>
